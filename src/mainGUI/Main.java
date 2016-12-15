23e7f4f71e0a2fa0bd66fbe6d56a1a893722b400
dustinrosebery <dustinrosebery>
2016-12-14 21:55:28 -0700

    Initial laptop commit

--- src/mainGUI/Main.java
+++ src/mainGUI/Main.java
@@ -1,27 +1,16 @@
-package UI;
-
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.image.Image;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    int WIDTH = 650;
-    int HEIGHT = 350;
-    int change = 1;
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Interface.fxml"));
-        primaryStage.setTitle("Castle Rush");
-        primaryStage.setScene(new Scene(root, WIDTH, HEIGHT));
+        primaryStage.setTitle("Castle Defense - Main Menu");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
