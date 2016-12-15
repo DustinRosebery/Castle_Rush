040cb52fb10b264757d073fcafb23c13e6e71545
dustinrosebery <dustinrosebery>
2016-12-14 22:35:51 -0700

    added background

--- src/mainGUI/Gui.java
+++ src/mainGUI/Gui.java
@@ -2,6 +2,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
@@ -32,14 +33,11 @@ public class Gui implements Initializable{
     {
         Parent root;
         Stage stage;
+        Main grip = new Main();
 
         if(e.getSource() == btn_newgame)
         {
-            //root = FXMLLoader.load(getClass().getResource("gameGUI.game.fxml"));
-            //stage = (Stage) btn_newgame.getScene().getWindow();
-            //Scene scene = new Scene(root);
-            //stage.setScene(scene);
-            //stage.show();
+            grip.startGame("gameUI.fxml");
         }
         else if(e.getSource() == btn_continue)
         {
--- src/mainGUI/Main.java
+++ src/mainGUI/Main.java
@@ -1,10 +1,16 @@
 import javafx.application.Application;
+import javafx.event.Event;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    private Stage primaryStage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -12,7 +18,32 @@ public class Main extends Application {
         primaryStage.setTitle("Castle Defense - Main Menu");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        this.primaryStage = primaryStage;
     }
+
+    public void startGame(String fxml) throws IOException
+    {
+
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        primaryStage.setTitle("Playing Castle_Rush");
+        primaryStage.setScene(new Scene(pane));
+        primaryStage.show();
+
+    }
+
+    /*private Parent replaceSceneContent(String fxml) throws Exception {
+        Parent page = (Parent) FXMLLoader.load(App.class.getResource(fxml), null, new JavaFXBuilderFactory());
+        Scene scene = stage.getScene();
+        if (scene == null) {
+            scene = new Scene(page, 700, 450);
+            scene.getStylesheets().add(App.class.getResource("demo.css").toExternalForm());
+            stage.setScene(scene);
+        } else {
+            stage.getScene().setRoot(page);
+        }
+        stage.sizeToScene();
+        return page;
+    }*/
 
     public static void main(String[] args) {
         launch(args);
